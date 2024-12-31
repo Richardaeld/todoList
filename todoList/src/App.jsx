0e12d1760b01d05993d1f57e19fdef15839b9086
Richard E. <richardaeld@gmail.com>
2024-12-31 02:46:57 +0000

    created NewTodoForm component

--- todoList/src/App.jsx
+++ todoList/src/App.jsx
@@ -1,22 +1,18 @@
 import { useState } from "react"
 import "./styles.css"
+import { NewTodoForm } from "./NewTodoFrom"
 
 export default function App() {
-  const [newItem, setNewItem] = useState("")
   const [todos, setTodos] = useState([])
   
-  function handleSubmit(e) {
-    e.preventDefault()
-
-    setTodos((currentTodos) => {
-      return [
-        ...currentTodos,
-        { id:crypto.randomUUID(), title: newItem, completed: false},
-      ]
-    })
-
-    setNewItem("")
-  }
+   function addTodo (title) {
+      setTodos((currentTodos) => {
+        return [
+          ...currentTodos,
+          { id:crypto.randomUUID(), title, completed: false},
+        ];
+      });
+   }
 
   function toggleTodo(id, completed) {
     setTodos(currentTodos => 
@@ -37,20 +33,10 @@ export default function App() {
 
   return (
     <>
-      <form onSubmit={handleSubmit} className="new-item-form">
-        <div className="form-row">
-          <label htmlFor="item">New Item</label>
-          <input 
-            value={newItem} 
-            onChange={e => setNewItem(e.target.value)} 
-            type="text" 
-            id="item"
-          />
-        </div>
-        <button className="btn">Add</button>
-      </form>
+      <NewTodoForm onSubmit={addTodo} />
       <h1 className="header">Todo List</h1>
       <ul className="list">
+        {todos.length === 0 && "No Todos"}
         {todos.map((todo) => {
           return (
           <li key={todo.id} >
