e5452dd729bf10392efae7072ef82c54672e1fff
Richard E. <richardaeld@gmail.com>
2025-01-01 02:27:44 +0000

    simplified some code

--- todoList/src/App.jsx
+++ todoList/src/App.jsx
@@ -8,7 +8,6 @@ export default function App() {
   const [todos, setTodos] = useState(() => {
     const localValue = localStorage.getItem("ITEMS")
     if (localValue == null) return []
-
     return JSON.parse(localValue)
   })
   
@@ -17,19 +16,17 @@ export default function App() {
   }, [todos])
 
    function addTodo (title) {
-      setTodos((currentTodos) => {
-        return [
+      setTodos((currentTodos) => [
           ...currentTodos,
           { id:crypto.randomUUID(), title, completed: false},
         ]
-      })
+      )
    }
 
   function toggleTodo(id, completed) {
     setTodos(currentTodos => 
       currentTodos.map(todo => {
         if (todo.id === id) return { ...todo, completed}
-
         return todo
       })
     )
--- todoList/src/TodoList.jsx
+++ todoList/src/TodoList.jsx
@@ -6,16 +6,14 @@ export function TodoList({ todos, toggleTodo, deleteTodo }) {
    return (
       <ul className="list">
          {todos.length === 0 && "No Todos"}
-         {todos.map(todo => {
-            return (
-               <TodoItem 
-                  {...todo} 
-                  key={todo.id} 
-                  toggleTodo={toggleTodo}
-                  deleteTodo={deleteTodo}
-               />
-            )
-         })}
+         {todos.map(todo => 
+            <TodoItem 
+               {...todo} 
+               key={todo.id} 
+               toggleTodo={toggleTodo}
+               deleteTodo={deleteTodo}
+            />
+         )}
       </ul>
    )
 }
